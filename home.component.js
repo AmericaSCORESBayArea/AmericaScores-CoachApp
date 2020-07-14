8cc4997404321798daf7cba4f23f34cb2608a940
cyanzon-mulesoft <cyanzon@mulesoft.com>
2020-07-14 15:40:15 -0300

    Improved over navigation tab overflow

--- home.component.js
+++ home.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {  BottomNavigationTab, BottomNavigation, Icon } from "@ui-kitten/components";
+import {  BottomNavigationTab, BottomNavigation, Icon, Button, TopNavigationAction, OverflowMenu, MenuItem } from "@ui-kitten/components";
 import TeamsScreen from "./src/TeamsScreen.component";
 import ActivitiesScreen from "./src/ActivitiesScreen.component";
 import AttendanceScreen from "./src/AttendanceScreen.component";
@@ -15,47 +15,47 @@ const TodayIcon = (props) => ( <Icon {...props} name='calendar-outline'/> );
 const StudentsIcon = (props) => ( <Icon {...props} name='people-outline'/> );
 
 const BottomTabBar = ({ navigation, state }) => (
-        <BottomNavigation 
-        selectedIndex={state.index} 
-        onSelect={index => navigation.navigate(state.routeNames[index])} >
-            <BottomNavigationTab title='Activities' icon={TodayIcon}/>
-            <BottomNavigationTab title='Teams' icon={SchoolIcon}/>
-            <BottomNavigationTab title='Students' icon={StudentsIcon}/>
-        </BottomNavigation>
+    <BottomNavigation 
+    selectedIndex={state.index} 
+    onSelect={index => navigation.navigate(state.routeNames[index])} >
+        <BottomNavigationTab title='Activities' icon={TodayIcon}/>
+        <BottomNavigationTab title='Teams' icon={SchoolIcon}/>
+        <BottomNavigationTab title='Students' icon={StudentsIcon}/>
+    </BottomNavigation>
 );
 
-const StackActivitiesView = createStackNavigator();
-const StackActivitiesViewScreen = ({navigation}) => (
-    <StackActivitiesView.Navigator>
-        <StackActivitiesView.Screen options={headerOptions} name='Activities' component={ActivitiesScreen} navigation={navigation}/>
-        <StackActivitiesView.Screen options={headerOptions} name="Attendance" component={AttendanceScreen} />
-        <StackActivitiesView.Screen options={headerOptions} name="Scan students" component={QRScanScreen}/>
-    </StackActivitiesView.Navigator>
+const Stack_Activities = createStackNavigator();
+const Stack_Activities_Navigation = ({navigation}) => (
+    <Stack_Activities.Navigator>
+        <Stack_Activities.Screen options={headerOptions} name='Activities' component={ActivitiesScreen} navigation={navigation}/>
+        <Stack_Activities.Screen options={headerOptions} name="Attendance" component={AttendanceScreen} />
+        <Stack_Activities.Screen options={headerOptions} name="Scan students QR" component={QRScanScreen}/>
+    </Stack_Activities.Navigator>
 );
 
-const StackTeamsView = createStackNavigator();
-const StackTeamsViewScreen = ({navigation}) => (
-    <StackTeamsView.Navigator>
-        <StackTeamsView.Screen name="Teams" component={TeamsScreen} options={headerOptions} navigation={navigation} />
-        <StackTeamsView.Screen name='Activities' component={ActivitiesScreen} options={headerOptions} navigation={navigation}/>
-        <StackTeamsView.Screen name="Attendance" component={AttendanceScreen} options={headerOptions} />
-        <StackTeamsView.Screen name="QRScreen" component={QRScanScreen} options={headerOptions}/>
-    </StackTeamsView.Navigator>
+const Stack_Teams = createStackNavigator();
+const Stack_Teams_Navigation = ({navigation}) => (
+    <Stack_Teams.Navigator>
+        <Stack_Teams.Screen name="Teams" component={TeamsScreen} options={headerOptions} navigation={navigation} />
+        <Stack_Teams.Screen name='Activities' component={ActivitiesScreen} options={headerOptions} navigation={navigation}/>
+        <Stack_Teams.Screen name="Attendance" component={AttendanceScreen} options={headerOptions} />
+        <Stack_Teams.Screen name="Scan students QR" component={QRScanScreen} options={headerOptions}/>
+    </Stack_Teams.Navigator>
 );
 
-const StackStudentsView = createStackNavigator();
-const StackStudentsScreen = ({navigation}) => (
-    <StackStudentsView.Navigator>
-        <StackTeamsView.Screen name="Students" component={StudentsScreen} options={headerOptions}/>
-    </StackStudentsView.Navigator>
+const Stack_Students = createStackNavigator();
+const Stack_Students_Navigation = ({navigation}) => (
+    <Stack_Students.Navigator>
+        <Stack_Students.Screen name="Students" component={StudentsScreen} options={headerOptions}/>
+    </Stack_Students.Navigator>
 );
 
 const { Navigator, Screen } = createBottomTabNavigator();
 const TabNavigator = ({navigation}) => (
     <Navigator tabBar={props => <BottomTabBar {...props} /> } >
-        <Screen name="ActivitiesViewStack" component={StackActivitiesViewScreen} />
-        <Screen name='TeamsStack' component={StackTeamsViewScreen} options={({route})=> ({ tittle: "Teams Seasons"})}/>
-        <Screen name='StudentsScreen' component={StackStudentsScreen}/>
+        <Screen name="ActivitiesStack" component={Stack_Activities_Navigation} />
+        <Screen name='TeamsStack' component={Stack_Teams_Navigation}/>
+        <Screen name='StudentsScreen' component={Stack_Students_Navigation}/>
     </Navigator>
   );
 
@@ -67,12 +67,41 @@ export const HomeScreen = ({navigation}) => {
     );
 }
 
-const headerOptions = {
-    headerStyle: {
-      backgroundColor: '#284de0',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
-}
+const OptionOverflowMenu = (navigation) => {
+    const [visoverflowMenuVisibleble, setOverflowMenuVisible] = React.useState(false);
+    const [nameValue, setNameValue] = React.useState();
+
+    const OptionsIcon = (props) => ( <Icon {...props} name='more-vertical-outline' /> );
+    const OptionButtons = () => (
+        <Button style={{flex:1}} accessoryRight={OptionsIcon} onPress={() => setOverflowMenuVisible(true)}/>
+    );
+
+    function menuItemOnPress(modalScreen) {
+        setOverflowMenuVisible(false);
+        navigation.navigate(modalScreen);
+    };
+
+    return (
+        <OverflowMenu
+        anchor={OptionButtons}
+        visible={visoverflowMenuVisibleble} 
+        placement={"bottom"} 
+        onBackdropPress={() => setOverflowMenuVisible(false)}>
+            <MenuItem title='Create Student' onPress={() => menuItemOnPress("CreateStudentModal")}/>
+            <MenuItem title='Add student' onPress={() => menuItemOnPress("AddStudentToTeamModal")}/>
+        </OverflowMenu>
+    );  
+};  
+
+//this.menuItemOnPress("AddStudentToTeamModal")
+const headerOptions =
+    ({navigation}) => ({
+        headerStyle: {
+          backgroundColor: '#336AFF',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        headerRight: () => <OptionOverflowMenu {...navigation}/>
+    })
