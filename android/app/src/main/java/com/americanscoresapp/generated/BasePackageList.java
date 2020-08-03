f5408f399fb1c9192e75599c053d188ef0cc04da
cyanzon-mulesoft <cyanzon@mulesoft.com>
2020-08-03 19:27:32 -0300

    Added changes and imrpovements

--- android/app/src/main/java/com/americanscoresapp/generated/BasePackageList.java
+++ android/app/src/main/java/com/americanscoresapp/generated/BasePackageList.java
@@ -7,10 +7,12 @@ import org.unimodules.core.interfaces.Package;
 public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
+        new expo.modules.appauth.AppAuthPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
+        new expo.modules.google.signin.GoogleSignInPackage(),
         new expo.modules.imageloader.ImageLoaderPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.lineargradient.LinearGradientPackage(),
---
+++ src/AppNavigator.Screen.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { LoginStackScreen, HomeRootStackScreen } from "../navigation.component";
+
+import { connect } from 'react-redux';
+import { syncSessions } from "./Redux/actions/Session.actions";
+import { bindActionCreators } from 'redux';
+
+class AppNavigator extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+    
+    render () {
+        const { Navigator, Screen } = createStackNavigator();
+
+        return (
+            <NavigationContainer>
+                <Navigator headerMode='none'>
+                    {
+                        (this.props.user.logged) ? 
+                        <Screen name="HomeRoot" component={HomeRootStackScreen}/>
+                        :
+                        <Screen name='Login' component={LoginStackScreen}/>
+                    }
+                </Navigator>
+            </NavigationContainer>
+        );
+    }
+}
+
+const mapStateToProps = state => ({ sessions: state.sessions, user: state.user  });
+  
+const ActionCreators = Object.assign( {}, { syncSessions } );
+  
+const mapDispatchToProps = dispatch => ({ actions: bindActionCreators(ActionCreators, dispatch) });
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppNavigator);
