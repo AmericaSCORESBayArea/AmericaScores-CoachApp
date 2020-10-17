870fed7e95e82b19fe3acc4a31517a9a77c1ee33
cyanzon <cyanzon@cyanzon-ltm.internal.salesforce.com>
2020-10-17 14:44:31 -0300

    minor changes

--- src/config/ApiConfig.js
+++ src/config/ApiConfig.js
@@ -1,6 +1,6 @@
 export const ApiConfig = {
-    baseUrl: 'https://salesforce-auth-api.us-e2.cloudhub.io/api',
-    dataApi: 'https://salesforce-data-api-proxy.us-e2.cloudhub.io/api',
-    clientId: '', //Keep it blank and only add them when compiling / testing, but DO NOT commit the keys as part of the code
-    clientSecret: ''
+    baseUrl: 'https://salesforce-auth-api-prod.us-e2.cloudhub.io/api',
+    dataApi: 'https://salesforce-data-api-proxy-prod.us-e2.cloudhub.io/api',
+    clientId: '87de15a992ad4510bf4f4a43d2f0e4bc', //Keep it blank and only add them when compiling / testing, but DO NOT commit the keys as part of the code
+    clientSecret: '415d5220915f4c81814978896a4AEcC3'
 }
