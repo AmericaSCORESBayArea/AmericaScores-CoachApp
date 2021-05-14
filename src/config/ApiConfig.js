2b3b81882018cb047848aefcf8b47cd9e7db3b71
Ignacio Lopez Scala <ignacio.a.lopezscala@gmail.com>
2021-05-14 12:00:08 -0300

    changed android version

--- src/config/ApiConfig.js
+++ src/config/ApiConfig.js
@@ -2,4 +2,5 @@ export const ApiConfig = {
     baseUrl: 'https://salesforce-auth-api-prod.us-e2.cloudhub.io/api',
     dataApi: 'https://salesforce-data-api-proxy-prod.us-e2.cloudhub.io/api',
     
+    
 }
