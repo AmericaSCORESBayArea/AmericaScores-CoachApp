ab25cd0dbaf428c957b1957b251e12f5422abde0
Lauti Rozen <lautirozen@gmail.com>
2021-07-16 12:46:29 -0300

    saving on redux the coach region selected

--- src/Redux/actions/SessionScreen.actions.js
+++ src/Redux/actions/SessionScreen.actions.js
@@ -1,8 +1,14 @@
 import { SEASONTITLE_CHANGE } from '../constants';
-
+import { COACHREGION_CHANGE } from '../constants';
 export function changeTitle(title) {
     return {
         type: SEASONTITLE_CHANGE,
         payload: title,
+    }
+}
+export function changeRegion(region) {
+    return {
+        type: COACHREGION_CHANGE,
+        payload: region,
     }
 }
