9070f9948854e078c5f8d0ad04b13dde31a2c21a
Lauti Rozen <lautirozen@gmail.com>
2021-07-16 12:45:47 -0300

    added region filter

--- src/TeamsScreen.component.js
+++ src/TeamsScreen.component.js
@@ -19,19 +19,19 @@ class TeamsScreen extends Component {
         this.state = {
             data: [],
             selectedData: [],
+            teamsRegion: "",
             value: "",
             nomatchModalVisibility: false,
             selectedIndex: "",
             displayedValue: "",
-            SFRegion: [],
-            SJRegion: [],
-            OARegion: [],
-            OtherRegion: [],
+            regions:['Other','San Francisco','San Jose','San Rafael','Oakland','Daly City','Hayward','Redwood City',
+            'San Francisco Civic Center','San Francisco Crocker','Alameda','Marin','San Mateo','Unrestricted',
+            'IFC-SF', 'Genesis'],
+            RegionSelected: "",//setting the region selected
         }
     }
 
     componentDidMount() {
-        
         const {user} = this.props.user;
         Axios.get(`${ApiConfig.dataApi}/coach/${user.ContactId}/teamseasons`, {
                 params: {
@@ -40,6 +40,8 @@ class TeamsScreen extends Component {
             })
         .then(response => {this.setState({data: response.data, selectedData: response.data}),this.regionFiltering(response.data)})
             .catch(e => console.log(e));
+        this.setState({displayedValue:this.state.regions[0]})//setting "basic" region filter with Other
+        this.setState({RegionSelected:"ASBA"})
     }
 
     setSearchBarValue(value) { this.setState({value: value}); }
@@ -47,11 +49,8 @@ class TeamsScreen extends Component {
     filter(item, query) { return item.TeamSeasonName.toLowerCase().includes(query.toLowerCase()) };
 
     setData(data) {
-        this.setState({selectedData: data})
-        this.setState({SFRegion:data.filter((value) =>(value.Region.match("IFC-SF")))})
-        this.setState({SJRegion:data.filter((value) =>(value.Region.match("San Jose")))})
-        this.setState({OARegion:data.filter((value) =>(value.Region.match("Oakland")))})
-        this.setState({OtherRegion:data.filter((value) =>(!value.Region.match("San Jose") && !value.Region.match("IFC-SF") && !value.Region.match("Oakland")))}) 
+        this.setState({selectedData: data.filter((value) =>(value.Region.match(this.state.RegionSelected)))})
+        this.setState({teamsRegion: data.filter((value) =>(value.Region.match(this.state.RegionSelected)))})
         if(!data.length){
             this.setState({nomatchModalVisibility: true})
         }else{
@@ -68,19 +67,31 @@ class TeamsScreen extends Component {
         this.props.navigation.navigate("Team Sessions", {teamSeasonId: teamSeasonId});
     };
 
-    /*SelectIndex(index){
+    SelectIndex(index){
         this.setState({selectedIndex: index});
         this.setState({displayedValue: this.state.regions[index.row]});
-    };*/
+        console.log(this.state.data.filter((value) =>(value.Region.match(this.state.regions[index.row]))))
+        this.setState({teamsRegion:this.state.data.filter((value) =>(value.Region.match(this.state.regions[index.row])))})
+        if(this.state.regions[index.row] === "Other"){
+            this.setState({RegionSelected:"ASBA"})
+        }else{
+            this.setState({RegionSelected:this.state.regions[index.row]})
+        }
+    }
 
     regionFiltering = (data) =>{
         if(data.length === 0){
             this.setState({nomatchModalVisibility: true})
         }else{
+            this.setState({teamsRegion:data.filter((value) =>(value.Region.match("Other")))})//saving sessions with region sf
+            this.setState({displayedValue:this.state.regions[0]})//setting "basic" region filter with Other
+            this.setState({RegionSelected:"ASBA"})
+            /*
             this.setState({SFRegion:data.filter((value) =>(value.Region.match("IFC-SF")))})
             this.setState({SJRegion:data.filter((value) =>(value.Region.match("San Jose")))})
             this.setState({OARegion:data.filter((value) =>(value.Region.match("Oakland")))})
             this.setState({OtherRegion:data.filter((value) =>(!value.Region.match("San Jose") && !value.Region.match("IFC-SF") && !value.Region.match("Oakland")))})
+        */
         }
     }
 
@@ -91,7 +102,7 @@ class TeamsScreen extends Component {
                 <Icon {...props} name='people-outline'/>
                 <Icon {...props} name='arrow-ios-forward-outline'/> 
             </View>);
-        /*let teamItem = ({ item, index }) => {
+        let teamItem = ({ item, index }) => {
             return(
                 <ListItem 
                     title={`${item.TeamSeasonName}`}
@@ -101,91 +112,39 @@ class TeamsScreen extends Component {
                     onPress={() => this.onPressTeam(item.TeamSeasonId)}
                 />
             )
-        };*/
-        let teamItemSF = ({ item, index }) => {
-                return(
-                    <ListItem 
-                        title={`${item.TeamSeasonName}`}
-                        style={{backgroundColor: "#C0E4F5"}}
-                        // description={`${item.description} ${index + 1}`}
-                        accessoryRight={rightArrowIconRender}
-                        onPress={() => this.onPressTeam(item.TeamSeasonId)}
-                    />
-            )
         };
-        const regionSF = (status) => (
+        const selectBox = () => (
+            <Select
+                label="Select a Region"
+                selectedIndex={this.state.selectedIndex}
+                style={{marginBottom:"2%", marginTop:"1%", marginLeft:"2%", marginRight:"2%"}}
+                value={this.state.displayedValue}
+                onSelect={index => this.SelectIndex(index)}>
+                {this.state.regions.map((title,i) =>
+                    <SelectItem key={title} title={title}/>
+                )}
+          </Select>
+        );
+        const regionName = (status) =>(
             (
-                (this.state.SFRegion.length !==0) &&
+                (this.state.teamsRegion.length !== 0 && this.state.RegionSelected.length !== 0) &&
                     <View style={{backgroundColor:"#52a5cc"}}>
-                        <Text style={{textAlign:"center"}} status={status} category='h6'>
-                            San Francisco
-                        </Text>
+                            <Text style={{textAlign:"center"}} status={status} category='h6'>
+                                {this.state.RegionSelected}
+                            </Text>
                     </View>
             )
         );
-        let teamItemSJ = ({ item, index }) => {
-            return(
-                <ListItem 
-                    title={`${item.TeamSeasonName}`}
-                    style={{backgroundColor: "#C0E4F5"}}
-                    // description={`${item.description} ${index + 1}`}
-                    accessoryRight={rightArrowIconRender}
-                    onPress={() => this.onPressTeam(item.TeamSeasonId)}
-                />
-            )
-        };
-        const regionSJ = (status) => (
+        const noMatchRegion = (status) =>(
             (
-                (this.state.SJRegion.length !==0) &&
-                    <View style={{backgroundColor:"#52a5cc"}}>
-                        <Text style={{textAlign:"center"}} status={status} category='h6'>
-                            San Jose
-                        </Text>
-                    </View>
+                (this.state.data.length !== 0 && this.state.teamsRegion.length === 0 && this.state.RegionSelected.length !== 0 && this.state.nomatchModalVisibility===false) &&
+                <Card style={{opacity: 0.9, backgroundColor:"#C0E4F5"}}>
+                    <Text category="s1" status={status} style={{alignSelf: 'center', backgroundColor:"#C0E4F5"}}>
+                        There are no active Teams for the selected Region.
+                    </Text>
+                </Card>
             )
         );
-        const regionOA = (status) => (
-            (
-                (this.state.OARegion.length !==0) &&
-                    <View style={{backgroundColor:"#52a5cc"}}>
-                        <Text style={{textAlign:"center"}} status={status} category='h6'>
-                            Oakland
-                        </Text>
-                    </View>
-            )
-        );
-        let teamItemOA = ({ item, index }) => {
-            return(
-                <ListItem 
-                    title={`${item.TeamSeasonName}`}
-                    style={{backgroundColor: "#C0E4F5"}}
-                    // description={`${item.description} ${index + 1}`}
-                    accessoryRight={rightArrowIconRender}
-                    onPress={() => this.onPressTeam(item.TeamSeasonId)}
-                />
-            )
-        };
-        const regionOther = (status) => (
-            (
-                (this.state.OtherRegion.length !==0) &&
-                    <View style={{backgroundColor:"#52a5cc"}}>
-                        <Text style={{textAlign:"center"}} status={status} category='h6'>
-                            Other
-                        </Text>
-                    </View>
-            )
-        )
-        let teamItemOther = ({ item, index }) => {
-            return(
-                <ListItem 
-                    title={`${item.TeamSeasonName}`}
-                    style={{backgroundColor: "#C0E4F5"}}
-                    // description={`${item.description} ${index + 1}`}
-                    accessoryRight={rightArrowIconRender}
-                    onPress={() => this.onPressTeam(item.TeamSeasonId)}
-                />
-            )
-        };
         const noMatch = (status) => (
             (
                 (this.state.nomatchModalVisibility) &&
@@ -196,7 +155,13 @@ class TeamsScreen extends Component {
                 </Card>
             )
         );
-        
+        const getImage = () =>{
+            if(this.props.sessionScreen.region === "IFC"){
+                return require('../assets/IFC-Logo.png');
+            }else{
+                return require('../assets/ASBA_Logo.png');
+            }
+        }
         return(
             <Layout style={{ flex: 1, justifyContent: 'center'}}>
                 <Autocomplete style={{margin:"2%"}}
@@ -207,47 +172,25 @@ class TeamsScreen extends Component {
                     size="large"
                     onChangeText={this.onChangeText} >
                 </Autocomplete>
+                {selectBox()}
                 <Divider/>
-                {/*{selectBox()}*/}
                 {noMatch("basic")}
-                <SafeAreaView  style={{backgroundColor:'#00000000',flex:1}}>
-                    <ImageBackground source={require('../assets/ASBA_Logo.png')} style={{flex:1, resizeMode: 'contain',opacity: 0.99, flexWrap: 'wrap'}}>
-                    {regionSF("basic")}
+                    <ImageBackground source={getImage()} style={{flex:1, resizeMode: 'contain',opacity: 0.99}}>
+                    {noMatchRegion("basic")}
+                    {regionName("basic")}
                     <List
-                        style={{opacity: 0.95,minWidth: "100%", flex:1, minHeight: "28%"}}
-                        data={this.state.SFRegion}
+                        style={{opacity: 0.95}}
+                        data={this.state.teamsRegion}
                         ItemSeparatorComponent={Divider}
-                        renderItem={teamItemSF}
-                    />
-                    {regionSJ("basic")}
-                    <List
-                        style={{opacity: 0.95,minWidth: "100%", flex:1, minHeight: "28%"}}
-                        data={this.state.SJRegion}
-                        ItemSeparatorComponent={Divider}
-                        renderItem={teamItemSJ}
-                    />
-                    {regionOA("basic")}
-                    <List
-                        style={{opacity: 0.95, minWidth: "100%", flex:1, minHeight: "28%"}}
-                        data={this.state.OARegion}
-                        ItemSeparatorComponent={Divider}
-                        renderItem={teamItemOA}
-                    />
-                    {regionOther("basic")}
-                    <List
-                        style={{opacity: 0.95, minWidth: "100%", flex:1, minHeight: "28%"}}
-                        data={this.state.OtherRegion}
-                        ItemSeparatorComponent={Divider}
-                        renderItem={teamItemOther}
+                        renderItem={teamItem}
                     />
                 </ImageBackground>
-                </SafeAreaView >
             </Layout>
         );
     };
 }
 
-const mapStateToProps = state => ({ sessions: state.sessions, user: state.user });
+const mapStateToProps = state => ({ sessions: state.sessions, user: state.user, sessionScreen: state.sessionScreen});
   
 const ActionCreators = Object.assign( {}, { syncSessions } );
   
