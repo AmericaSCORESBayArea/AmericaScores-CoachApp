d6e865673469d7f82d14ef32089de17cf19f696a
Lauti Rozen <lautirozen@gmail.com>
2021-09-07 22:25:26 -0300

    added upload image option

--- src/components/ReportModal.component.js
+++ src/components/ReportModal.component.js
@@ -1,6 +1,7 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,useCallback} from 'react';
 import { Modal, Card, Text, Button, Layout, Input, Select, SelectItem, Icon } from '@ui-kitten/components';
-import { ImageBackground,Keyboard, ScrollView, Alert } from 'react-native';
+import { ImageBackground,Keyboard, ScrollView, Alert, Dimensions, Image } from 'react-native';
+import DocumentPicker from "react-native-document-picker";
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import { ApiConfig } from '../config/ApiConfig';
@@ -17,9 +18,13 @@ export const CreateReportModal = ({navigation}) => {
     const [visible, setVisible] = React.useState(true);
     const multilineInputState = useInputState();
     const [responseStatusModal, setResponseStatusModal] = React.useState(false);
+    const [reponseStatusUnsuccessModal,setReponseStatusUnsuccessModal] = React.useState(false);
     const [selectedIndex, setSelectedIndex] = React.useState();
     const [displayedValue, setDisplayedValue] = React.useState(data[0]);
     const [keyboardSize, setKeyboardSize] = React.useState(0);
+    const [uploadUrl, setUploadUrl] = React.useState(null);
+    const [url,setUrl] = React.useState('');
+
     useEffect(() => {
         Keyboard.addListener("keyboardDidShow", (e) => {
             setKeyboardSize(e.endCoordinates.height)
@@ -34,6 +39,12 @@ export const CreateReportModal = ({navigation}) => {
             Keyboard.removeAllListeners("keyboardDidHide");
         })
     }, [])
+    useEffect(() => {
+        console.log(url)
+        if(url !== undefined && url.length !== 0){
+            postMessageToChannelWithImage();
+        }
+    }, [url]);
     const message=
         { 
             'Content-type': 'application/json',
@@ -69,6 +80,55 @@ export const CreateReportModal = ({navigation}) => {
                     },
                 ]
         }
+    const messagewithImage= {
+        'Content-type': 'application/json',
+        "blocks": [
+            {
+                "type": "divider",
+                "block_id": "divider1"
+            },
+            {
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Coach Name*"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Option*"
+                    },
+                    {
+                        "type": "plain_text",
+                        "text": useSelector(state => state.user.user.FirstName) + " " + useSelector(state => state.user.user.LastName)
+                    },
+                    {
+                        "type": "plain_text",
+                        "text": displayedValue
+                    }
+                ],
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text":  "*Message:* " + " " + multilineInputState.descriptionvalue,
+                }
+            },{
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text":  "*ImageURL:* " + " " + url,
+                },
+                "accessory": {
+                    "type": "image",
+                    "image_url": url,
+                    "alt_text": url
+                }
+            },
+            {
+                "type": "divider",
+                "block_id": "divider2"
+            },
+        ]
+    }
     const postMessageToChannel = () => {axios.post(ApiConfig.slackWebHook,message)
     .then(function (response) {
         console.log(response)
@@ -76,8 +136,24 @@ export const CreateReportModal = ({navigation}) => {
       })
       .catch(function (error) {
         console.log(error);
+        setVisible(false);
+        setResponseStatusModal(false);
+        setReponseStatusUnsuccessModal(true);
       });
     }
+
+    const postMessageToChannelWithImage = () => {console.log(url),axios.post(ApiConfig.slackWebHook,messagewithImage)
+        .then(function (response) {
+            console.log(response)
+            closeModal();
+          })
+          .catch(function (error) {
+            console.log(error);
+            setVisible(false);
+            setResponseStatusModal(false);
+            setReponseStatusUnsuccessModal(true);
+          });
+        }
 
     function closeModal() {
         setVisible(false);
@@ -89,6 +165,26 @@ export const CreateReportModal = ({navigation}) => {
         navigation.goBack();
     }
 
+    function toggleUnsuccessNotificationOff(){
+        setReponseStatusUnsuccessModal(false);
+        setVisible(true);
+    }
+    async function asyncCall() {
+        const data = new FormData()
+        data.append('file', uploadUrl[0])
+        data.append('upload_preset', ApiConfig.cloudName)
+        data.append("cloud_name", ApiConfig.cloudName)
+        fetch(ApiConfig.cloudinaryURL, {
+            method: "post",
+            body: data
+            }).then(res => res.json()).
+                then(data => {
+                    setUrl(data.secure_url);
+                }).catch(err => {
+                    console.log(err)
+                    Alert.alert('Oops',"An Error occured while uploading the Image. Please try again.")
+                }) 
+      }
     function createReport() {
         if(multilineInputState.descriptionvalue === ''){
             Alert.alert(
@@ -100,7 +196,12 @@ export const CreateReportModal = ({navigation}) => {
                 { cancelable: false }
               );
         }else{
-            postMessageToChannel();
+            setReponseStatusUnsuccessModal(false);
+            if(uploadUrl !== null){
+                asyncCall();
+            }else{
+                postMessageToChannel();
+            }
         }
     }
     function cancellReport() {
@@ -110,12 +211,12 @@ export const CreateReportModal = ({navigation}) => {
 
     const Footer = (props) => (
         <Layout {...props}>
-            <Button appearance='ghost' status='danger' onPress={() => cancellReport()}>
-                Cancel
-            </Button>
-            <Button onPress={() => createReport()}>
-                Send
-            </Button>
+                <Button appearance='ghost' status='danger' onPress={() => cancellReport()}>
+                    Cancel
+                </Button>
+                <Button onPress={() => createReport()}>
+                    Send
+                </Button>
         </Layout>
     );
     
@@ -135,6 +236,30 @@ export const CreateReportModal = ({navigation}) => {
             />
         </Layout>
     );
+    const UnsuccessHeader = (props) => (
+        <Layout {...props}>
+             <ImageBackground
+                resizeMode="contain"
+                style={{height:100, width:100, alignSelf:"center"}}
+                source={require('../../assets/error_icon.png')}
+            />
+        </Layout>
+    );
+    const pickFile = useCallback(async () => {
+        try {
+          const res = await DocumentPicker.pick({
+            type: [DocumentPicker.types.images],
+          });
+          console.log(res)
+          setUploadUrl(res)
+        } catch (err) {
+          if (DocumentPicker.isCancel(err)) {
+            console.log("User cancelled the picker, exit any dialogs or menus and move on");
+          } else {
+            throw err;
+          }
+        }
+      }, []);
     const problemIcon = (props) => (<Icon {...props} name='alert-triangle-outline'/>)
     const improvementIcon = (props) => (<Icon {...props} name='message-square-outline'/>)
     const questionIcon = (props) => (<Icon {...props} name='question-mark-circle-outline'/>) 
@@ -143,28 +268,28 @@ export const CreateReportModal = ({navigation}) => {
         setSelectedIndex(index)
         setDisplayedValue(data[index.row])
     }
+    const windowHeight = Dimensions.get('window').height;
     return(
         <React.Fragment>
             <Modal
                 visible={visible}
                 onBackdropPress={() => cancellReport()}
-                animationType="slide"
-                style={{maxWidth:'90%', marginBottom: keyboardSize}}>
+                style={{maxWidth:'90%',height:windowHeight,marginBottom: keyboardSize}}>
                 <ScrollView>
-                <Card disabled={true} style={{marginBottom: keyboardSize }} header={Header} footer={Footer}>
+                <Card disabled={true} style={{marginBottom: keyboardSize}} header={Header} footer={Footer}>
                     <ScrollView>
-                    <Select
-                        label='Select an option'
-                        placeholder='Select an option'
-                        value={displayedValue}
-                        size='large'
-                        selectedIndex={selectedIndex}
-                        style={{marginBottom:"8%"}}
-                        onSelect={index => selectIndex(index)}>
-                        <SelectItem title='Report a problem' accessoryLeft={problemIcon} />
-                        <SelectItem title='Suggest an improvement' accessoryLeft={improvementIcon} />
-                        <SelectItem title='Ask a question' accessoryLeft={questionIcon} />
-                    </Select>
+                        <Select
+                            label='Select an option'
+                            placeholder='Select an option'
+                            value={displayedValue}
+                            size='large'
+                            selectedIndex={selectedIndex}
+                            style={{marginBottom:"8%"}}
+                            onSelect={index => selectIndex(index)}>
+                            <SelectItem title='Report a problem' accessoryLeft={problemIcon} />
+                            <SelectItem title='Suggest an improvement' accessoryLeft={improvementIcon} />
+                            <SelectItem title='Ask a question' accessoryLeft={questionIcon} />
+                        </Select>
                         <Input
                             label='Details'
                             multiline={true}
@@ -172,6 +297,10 @@ export const CreateReportModal = ({navigation}) => {
                             placeholder='Tell us more about it'
                             {...multilineInputState}
                         />
+                            <Button appearance='ghost' onPress={pickFile}>
+                                Choose File
+                            </Button>
+                            {uploadUrl && <Image source={{uri: uploadUrl[0].uri}} style={{height:80, width:80}}/>}
                     </ScrollView>
                 </Card>
                 </ScrollView>
@@ -181,12 +310,23 @@ export const CreateReportModal = ({navigation}) => {
             style={{width:'80%'}}
             onBackdropPress={() => toggleNotificationOff()}>
                 <Card disabled={true} header={SuccessHeader}>
-                <Text status={'success'} style={{ margin: 15, alignSelf:"center"}}>Thanks for your time.</Text> 
+                    <Text status={'success'} style={{ margin: 15, alignSelf:"center"}}>Thanks for your time.</Text> 
                     <Button appearance='outline' size={'small'} onPress={() => toggleNotificationOff()} status={'success'}>
                         OK
-                </Button>
-            </Card>
-        </Modal>
+                    </Button>
+                </Card>
+            </Modal>
+            <Modal
+            visible={reponseStatusUnsuccessModal}
+            style={{width:'80%'}}
+            onBackdropPress={() => toggleNotificationOff()}>
+                <Card disabled={true} header={UnsuccessHeader}>
+                    <Text status={'danger'} style={{ margin: 15, alignSelf:"center"}}>Something went wrong. Please, try again.</Text> 
+                    <Button appearance='outline' size={'small'} onPress={() => toggleUnsuccessNotificationOff()} status={'danger'}>
+                        OK
+                    </Button>
+                </Card>
+            </Modal>
     </React.Fragment>
     );
 }
