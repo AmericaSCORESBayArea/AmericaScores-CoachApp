e523fc898bcb5a1df3433c19473b6e5bd417baed
lautirozen <lautirozen@gmail.com>
2022-11-11 17:18:05 -0300

    added in app messaging

--- App.js
+++ App.js
@@ -1,33 +1,42 @@
-import React from 'react';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import AppNavigator from './src/AppNavigator.Screen';
-import * as AppAuth from 'expo-app-auth';
-import { Provider } from 'react-redux';
+import React, { useEffect } from "react";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
+import AppNavigator from "./src/AppNavigator.Screen";
+import * as AppAuth from "expo-app-auth";
+import { Provider } from "react-redux";
+import inAppMessaging from "@react-native-firebase/in-app-messaging";
+import { default as theme } from "./assets/ASBA_Theme_Light_v1.json"; // <-- Import app theme
+import { ApiConfig } from "./src/config/ApiConfig";
 
-import { default as theme } from './assets/ASBA_Theme_Light_v1.json'; // <-- Import app theme
-import {ApiConfig} from "./src/config/ApiConfig";
-
-import configureStore from './src/config/ConfigureStore';
-import Axios from 'axios';
+import configureStore from "./src/config/ConfigureStore";
+import Axios from "axios";
 
 // When configured correctly, URLSchemes should contain your REVERSED_CLIENT_ID
 const { URLSchemes } = AppAuth;
-const store = configureStore()
+const store = configureStore();
 
 export default function App() {
-  Axios.defaults.headers.common['client_id'] = ApiConfig.clientId;
-  Axios.defaults.headers.common['client_secret'] = ApiConfig.clientSecret;
-
+  Axios.defaults.headers.common["client_id"] = ApiConfig.clientId;
+  Axios.defaults.headers.common["client_secret"] = ApiConfig.clientSecret;
+  useEffect(() => {
+    const dismissFirebaseModal = async () => {
+      await inAppMessaging().setMessagesDisplaySuppressed(true);
+    };
+    dismissFirebaseModal();
+  }, []);
   return (
     <>
-        <IconRegistry icons={EvaIconsPack}/>
-        <ApplicationProvider store={configureStore} {...eva} theme={{...eva.light, ...theme}}>
-          <Provider store={store}>
-            <AppNavigator/>
-          </Provider>    
-        </ApplicationProvider>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider
+        store={configureStore}
+        {...eva}
+        theme={{ ...eva.light, ...theme }}
+      >
+        <Provider store={store}>
+          <AppNavigator />
+        </Provider>
+      </ApplicationProvider>
     </>
   );
 }
