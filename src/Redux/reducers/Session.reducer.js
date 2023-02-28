9186cf88b995c5c682921a8c742e9f072bf2f477
Lautaro Rozenblum <lautirozen@gmail.com>
2023-02-28 15:59:26 -0300

    updated redux sessions information

--- src/Redux/reducers/Session.reducer.js
+++ src/Redux/reducers/Session.reducer.js
@@ -1,19 +1,23 @@
-import { SYNC, UPDATE_ATTENDANCE } from '../constants';
+import { SYNC, UPDATE_ATTENDANCE, SYNC_SESSIONS_TAB } from "../constants";
 
-const initialState = { sessions: [] };
+const initialState = { sessions: [], sessions_tab: [] };
 
 const sessionReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case SYNC:
-            return { ...state, sessions: action.payload};
-        case UPDATE_ATTENDANCE:
-            const index = state.sessions.findIndex(session => session.TeamSeasonId !== action.payload.teamSeasonId) 
-            const newSessions = [...state.sessions];
-            newSessions[index].Enrollments = action.payload.enrollments;
+  switch (action.type) {
+    case SYNC:
+      return { ...state, sessions: action.payload };
+    case SYNC_SESSIONS_TAB:
+      return { ...state, sessions_tab: action.payload };
+    case UPDATE_ATTENDANCE:
+      const index = state.sessions.findIndex(
+        (session) => session.TeamSeasonId !== action.payload.teamSeasonId
+      );
+      const newSessions = [...state.sessions];
+      newSessions[index].Enrollments = action.payload.enrollments;
 
-            return { ...state, sessions: newSessions }
-        default:
-            return state;
-    }
-}
+      return { ...state, sessions: newSessions };
+    default:
+      return state;
+  }
+};
 export default sessionReducer;
