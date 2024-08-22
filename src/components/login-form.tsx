ee72bc686a70e6fc74a5eaa2ea622240d0a3e1d8
za-zohaib <za.zohaibarshad@gmail.com>
2024-08-22 21:20:38 +0500

    WIP

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,10 +1,13 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import React from 'react';
+import { ImageBackground, StyleSheet } from 'react-native';
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
-
 import { Button, ControlledInput, Text, View } from '@/ui';
+import { Image } from 'expo-image';
+import { SafeAreaView } from 'moti';
+import { opacity } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
 
 const schema = z.object({
   name: z.string().optional(),
@@ -31,37 +34,118 @@ export const LoginForm = ({ onSubmit = () => {} }: LoginFormProps) => {
     resolver: zodResolver(schema),
   });
   return (
-    <View className="flex-1 justify-center p-4">
-      <Text testID="form-title" className="pb-6 text-center text-2xl">
-        Sign In
-      </Text>
+    // <View className="flex-1 justify-center p-4s">
+    //   <Text testID="form-title" className="pb-6 text-center text-2xl">
+    //     Sign In
+    //   </Text>
 
-      <ControlledInput
-        testID="name"
-        control={control}
-        name="name"
-        label="Name"
-      />
+    //   <ControlledInput
+    //     testID="name"
+    //     control={control}
+    //     name="name"
+    //     label="Name"
+    //   />
 
-      <ControlledInput
-        testID="email-input"
-        control={control}
-        name="email"
-        label="Email"
-      />
-      <ControlledInput
-        testID="password-input"
-        control={control}
-        name="password"
-        label="Password"
-        placeholder="***"
-        secureTextEntry={true}
-      />
-      <Button
-        testID="login-button"
-        label="Login"
-        onPress={handleSubmit(onSubmit)}
-      />
-    </View>
+    //   <ControlledInput
+    //     testID="email-input"
+    //     control={control}
+    //     name="email"
+    //     label="Email"
+    //   />
+    //   <ControlledInput
+    //     testID="password-input"
+    //     control={control}
+    //     name="password"
+    //     label="Password"
+    //     placeholder="***"
+    //     secureTextEntry={true}
+    //   />
+    //   <Button
+    //     testID="login-button"
+    //     label="Login"
+    //     onPress={handleSubmit(onSubmit)}
+    //   />
+    // </View>
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // <View style={styles.container}>
+    // <SafeAreaView style={{ flex: 1 }}>
+    <ImageBackground
+      source={require('../../assets/LogInBackground.jpeg')}
+      style={styles.backgroundImageContainer}
+    >
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>Log in</Text>
+          <Text>America Scores Attendence App</Text>
+        </View>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.image}
+            source={require('../../assets/ASBA_Logo.jpg')}
+            contentFit="contain"
+            // transition={1000}
+          />
+        </View>
+        <View style={styles.signInContainer}>
+          <Text>Login with Google</Text>
+          <Text>Login with Phone Number</Text>
+        </View>
+      </View>
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  backgroundImageContainer: {
+    flex: 1,
+    flexDirection: 'column',
+
+    justifyContent: 'center',
+  },
+  container: {
+    height: '80%',
+    // borderColor: 'brown',
+    // borderWidth: 3,
+    alignItems: 'center',
+
+    marginHorizontal: 10,
+  },
+  textContainer: {
+    width: '100%',
+    height: '8%',
+    justifyContent: 'center',
+
+    paddingLeft: 10,
+    opacity: 0.6,
+
+    backgroundColor: 'white',
+  },
+  title: {
+    fontWeight: 'bold',
+  },
+
+  image: {
+    flex: 1,
+    width: '100%',
+  },
+  signInContainer: {
+    height: '10%',
+    width: '100%',
+
+    // borderColor: 'blue',
+    // borderWidth: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    marginHorizontal: 10,
+  },
+  imageContainer: {
+    height: '82%',
+
+    width: '100%',
+    opacity: 0.6,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
