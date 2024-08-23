bc00156b6831fc2b0b89e67533ecf98ce88e976b
za-zohaib <za.zohaibarshad@gmail.com>
2024-08-23 19:27:39 +0500

    WIP on modal

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,14 +1,13 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { Image } from 'expo-image';
+import React, { useCallback, useRef, useState } from 'react';
 import type { SubmitHandler } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
+import { Button, ImageBackground, Pressable, StatusBar } from 'react-native';
 import * as z from 'zod';
-import { Button, ControlledInput, Text, View } from '@/ui';
-import { Image } from 'expo-image';
-import { SafeAreaView } from 'moti';
-import { opacity } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
-
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { colors, FocusAwareStatusBar, SafeAreaView, Text, View } from '@/ui';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 const schema = z.object({
   name: z.string().optional(),
   email: z
@@ -33,119 +32,110 @@ export const LoginForm = ({ onSubmit = () => {} }: LoginFormProps) => {
   const { handleSubmit, control } = useForm<FormType>({
     resolver: zodResolver(schema),
   });
+
+  const [signInOptions, setSignInOptions] = useState<string>('');
+
+  // ref
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+
+  const signInOptionsHandler = (option: string) => {
+    console.log('options : ', option);
+    setSignInOptions(option);
+    bottomSheetRef.current?.expand();
+  };
+  const handleClosePress = () => {
+    bottomSheetRef.current?.close();
+    setSignInOptions('');
+  };
   return (
-    // <View className="flex-1 justify-center p-4s">
-    //   <Text testID="form-title" className="pb-6 text-center text-2xl">
-    //     Sign In
-    //   </Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusAwareStatusBar />
 
-    //   <ControlledInput
-    //     testID="name"
-    //     control={control}
-    //     name="name"
-    //     label="Name"
-    //   />
+      {signInOptions === '' && (
+        <ImageBackground
+          source={require('../../assets/LogInBackground.jpeg')}
+          className="flex-1 flex-col fixed justify-center border-b-blue-800 boder-2 "
+        >
+          <View className=" h-4/5 items-center mx-2.5">
+            <View
+              // style={styles.textContainer}
+              className="h-[8%] w-full justify-center pl-2.5 opacity-60 bg-white z-50"
+            >
+              <Text className="font-bold opacity-100 ">Log in</Text>
+              <Text className="font-medium opacity-100">
+                America Scores Attendence App
+              </Text>
+            </View>
+            <View className="h-[80%] w-full opacity-60 bg-white items-center justify-center object-contain">
+              <Image
+                className="h-full w-full"
+                source={require('../../assets/ASBA_Logo.jpg')}
 
-    //   <ControlledInput
-    //     testID="email-input"
-    //     control={control}
-    //     name="email"
-    //     label="Email"
-    //   />
-    //   <ControlledInput
-    //     testID="password-input"
-    //     control={control}
-    //     name="password"
-    //     label="Password"
-    //     placeholder="***"
-    //     secureTextEntry={true}
-    //   />
-    //   <Button
-    //     testID="login-button"
-    //     label="Login"
-    //     onPress={handleSubmit(onSubmit)}
-    //   />
-    // </View>
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    // <View style={styles.container}>
-    // <SafeAreaView style={{ flex: 1 }}>
-    <ImageBackground
-      source={require('../../assets/LogInBackground.jpeg')}
-      style={styles.backgroundImageContainer}
-    >
-      <View style={styles.container}>
-        <View style={styles.textContainer}>
-          <Text style={styles.title}>Log in</Text>
-          <Text>America Scores Attendence App</Text>
-        </View>
-        <View style={styles.imageContainer}>
-          <Image
-            style={styles.image}
-            source={require('../../assets/ASBA_Logo.jpg')}
-            contentFit="contain"
-            // transition={1000}
-          />
-        </View>
-        <View style={styles.signInContainer}>
-          <Text>Login with Google</Text>
-          <Text>Login with Phone Number</Text>
-        </View>
-      </View>
-    </ImageBackground>
+                // transition={1000}
+              />
+            </View>
+            <View className="h-[12%] w-full items-center justify-center   bg-white mx-2.5 ">
+              <Pressable
+                className="flex-row w-3/6  justify-between my-1.5 "
+                onPress={() => {
+                  signInOptionsHandler('Google');
+                }}
+              >
+                <FontAwesome
+                  name="google"
+                  size={22}
+                  color={colors.danger[700]}
+                />
+                <Text className="text-danger-700 font-bold ">
+                  SIGN IN WITH GOOGLE
+                </Text>
+              </Pressable>
+              <View className="flex-row w-3/6 justify-between my-1.5 ">
+                <FontAwesome
+                  name="phone"
+                  size={22}
+                  color={colors.primary[700]}
+                />
+
+                <Text className="text-primary-700 font-bold ">
+                  SIGN IN WITH PHONE
+                </Text>
+              </View>
+            </View>
+          </View>
+        </ImageBackground>
+      )}
+      {signInOptions === 'Google' && (
+        <ImageBackground
+          source={require('../../assets/LogInBackground.jpeg')}
+          className="flex-1 flex-col fixed justify-center border-b-blue-800 boder-2 "
+        >
+          <View className="flex-1 p-6 ">
+            {/* <Button title="Close Sheet" onPress={handleClosePress} /> */}
+            <BottomSheet
+              ref={bottomSheetRef}
+              onChange={handleSheetChanges}
+              snapPoints={['90%']}
+            >
+              <FontAwesome
+                name="close"
+                size={24}
+                color="black"
+                className="self-end mr-1"
+                onPress={handleClosePress}
+              />
+              <BottomSheetView className="flex-1  items-center">
+                <Text>Awesome 🎉</Text>
+              </BottomSheetView>
+            </BottomSheet>
+          </View>
+        </ImageBackground>
+      )}
+    </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  backgroundImageContainer: {
-    flex: 1,
-    flexDirection: 'column',
-
-    justifyContent: 'center',
-  },
-  container: {
-    height: '80%',
-    // borderColor: 'brown',
-    // borderWidth: 3,
-    alignItems: 'center',
-
-    marginHorizontal: 10,
-  },
-  textContainer: {
-    width: '100%',
-    height: '8%',
-    justifyContent: 'center',
-
-    paddingLeft: 10,
-    opacity: 0.6,
-
-    backgroundColor: 'white',
-  },
-  title: {
-    fontWeight: 'bold',
-  },
-
-  image: {
-    flex: 1,
-    width: '100%',
-  },
-  signInContainer: {
-    height: '10%',
-    width: '100%',
-
-    // borderColor: 'blue',
-    // borderWidth: 3,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    marginHorizontal: 10,
-  },
-  imageContainer: {
-    height: '82%',
-
-    width: '100%',
-    opacity: 0.6,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
