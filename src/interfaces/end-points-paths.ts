59ca8b4da22bddbc5d363c09e9a81b25b3c737aa
za-zohaib <za.zohaibarshad@gmail.com>
2024-09-25 14:11:51 +0500

    end points path fix

--- src/interfaces/end-points-paths.ts
+++ src/interfaces/end-points-paths.ts
@@ -1,6 +1,6 @@
 export enum EndpointPaths {
-  AUTH_SIGNUP = '/auth/signup',
-  AUTH_LOGIN = '/auth/login',
+  AUTH_SIGNUP = 'auth/signup',
+  AUTH_LOGIN = 'auth/login',
   COACH_REGION = 'coach/003UQ00000EiAy9YAF/regions',
-  TEAM_SEASON = '/teamSeasons?date=2023-08-05&isWithinDateRange=true',
+  TEAM_SEASON = 'teamSeasons?date=2023-08-05&isWithinDateRange=true',
 }
